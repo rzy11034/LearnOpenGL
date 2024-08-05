9d06e001b99adb6d1546696a6bd2b25f0e848458
BNC204-01 <rzy11034@163.com>
2024-08-05 18:01:53 +0800

    Signed-off-by: BNC204-01 <rzy11034@163.com>

--- Source/DeepStar.OpenGL.Model.pas
+++ Source/DeepStar.OpenGL.Model.pas
@@ -13,6 +13,7 @@ uses
   DeepStar.DSA.Linear.ArrayList,
   DeepStar.OpenGL.Utils,
   DeepStar.OpenGL.GLAD_GL,
+  DeepStar.OpenGL.GLM,
   DeepStar.OpenGL.Texture,
   DeepStar.OpenGL.Shader,
   DeepStar.OpenGL.Mesh,
@@ -28,7 +29,7 @@ type
     _textures_loaded: TList_TTexture;
     _meshes: TList_TMesh;
 
-    function __ProcessMesh(aMesh: PaiMesh; const scene: PaiScene):TMesh;
+    function __ProcessMesh(mesh: PaiMesh; const scene: PaiScene):TMesh;
 
     procedure __LoadModel(fileName: string);
 
@@ -95,13 +96,16 @@ begin
   __ProcessNode(scene^.mRootNode, scene);
 end;
 
-function TModel.__ProcessMesh(aMesh: PaiMesh; const scene: PaiScene): TMesh;
+function TModel.__ProcessMesh(mesh: PaiMesh; const scene: PaiScene): TMesh;
 var
   vertices: TList_TVertex;
   indices: TList_Gluint;
   textures: TList_TTexture;
-  i: uint;
   vertex: TVertex;
+  i: cardinal;
+  tempVec3: TVec3;
+  tempVec2: TVec2;
+  face: TaiFace;
 begin
   // data to fill
   vertices := TList_TVertex.Create;
@@ -109,20 +113,74 @@ begin
   textures := TList_TTexture.Create;
 
   // 遍历每个网格的顶点
-  for i := uint(0) to aMesh^.mNumVertices-1 do
+  for i := 0 to mesh^.mNumVertices - 1 do
   begin
     vertex := Default(TVertex);
+    // 我们声明一个占位符向量，因为assimp使用自己的vector类，
+    // 不能直接转换为glm的vec3类，所以我们首先将数据传输到这个占位符 TGLM.vec3。
+    tempVec3 := TGLM.Vec3(0);
+
+    // position
+    tempVec3.x := mesh^.mVertices[i].x;
+    tempVec3.y := mesh^.mVertices[i].y;
+    tempVec3.z := mesh^.mVertices[i].z;
+    vertex.Position := tempVec3;
+
+    // normals
+    if mesh^.mNormals[i] <> nil then
+    begin
+      tempVec3.x := mesh^.mNormals[i].x;
+      tempVec3.y := mesh^.mNormals[i].y;
+      tempVec3.z := mesh^.mNormals[i].z;
+      vertex.Normal := tempVec3;
+    end;
+
+    // texture coordinates
+    if mesh^.mTextureCoords[0] <> nil then
+    begin
+      tempVec2 := TGLM.Vec2(0);
+
+      //一个顶点最多可以包含8个不同的纹理坐标。因此，我们假设我们不会
+      //使用顶点可以有多个纹理坐标的模型，所以我们总是取第一个集合(0)。
+      tempVec2.x := mesh^.mTextureCoords[0][i].x;
+      tempVec2.y := mesh^.mTextureCoords[0][i].y;
+      vertex.TexCoords := tempVec2;
+
+      // tangent
+      tempVec3.x := mesh^.mTangents[i].x;
+      tempVec3.y := mesh^.mTangents[i].y;
+      tempVec3.z := mesh^.mTangents[i].z;
+      vertex.Tangent := tempVec3;
+
+      // bitangent
+      tempVec3.x := mesh^.mBitangents[i].x;
+      tempVec3.y := mesh^.mBitangents[i].y;
+      tempVec3.z := mesh^.mBitangents[i].z;
+      vertex.Bitangent := tempVec3;
+    end
+    else
+      vertex.TexCoords := TGLM.Vec2(0);
+
+    vertices.AddLast(vertex);
   end;
 
+  //现在遍历每个网格的面 (面是一个网格的三角形) 并检索相应的顶点索引。
+  for i := 0 to mesh^.mNumFaces -1 do
+  begin
+    face := mesh^.mFaces[i];
+
+    // 检索脸的所有索引并将它们存储在索引向量中
+
+  end;
 end;
 
 procedure TModel.__ProcessNode(node: PaiNode; const scene: PaiScene);
 var
   mesh: PaiMesh;
-  i: uint;
+  i: cardinal;
 begin
   // 处理位于当前节点的每个网格
-  for i := uint(0) to node^.mNumChildren - 1 do
+  for i := 0 to node^.mNumChildren - 1 do
   begin
     // 节点对象只包含索引来索引场景中的实际对象。
     // 场景包含所有的数据，节点只是保持东西的组织(像节点之间的关系)。
@@ -132,9 +190,9 @@ begin
   end;
 
   //在我们处理完所有的网格(如果有的话)之后，我们递归地处理每个子节点
-  for i := uint(0) to node^.mNumChildren - 1 do
+  for i := 0 to node^.mNumChildren - 1 do
   begin
-
+    __ProcessNode(node^.mChildren[i], scene);
   end;
 end;
 
--- Source/LearnOpenGL.Main.pas
+++ Source/LearnOpenGL.Main.pas
@@ -26,156 +26,17 @@ implementation
 uses
   Case02_06_02_Multiple_Lights_Exercise1;
 
-type
-  PVertexObj = ^TVertexObj;
-  TVertexObj = object(TObj_Base)
-    Position: TVec3;
-    Normal: TVec3;
-    TexCoords: TVec2;
-    Tangent: TVec3;
-    Bitangent: TVec3;
-    BoneIDs: array[0..MAX_BONE_INFLUENCE - 1] of integer;
-    Weights: array[0..MAX_BONE_INFLUENCE - 1] of single;
-
-    constructor Init;
-    destructor Done; virtual;
-
-    class function Create: TVertexObj; static;
-    class function CreatePtr: PVertexObj; static;
-  end;
-
-  PVertexRec = ^TVertexRec;
-  TVertexRec = record
-    Position: TVec3;
-    Normal: TVec3;
-    TexCoords: TVec2;
-    Tangent: TVec3;
-    Bitangent: TVec3;
-    BoneIDs: array[0..MAX_BONE_INFLUENCE - 1] of integer;
-    Weights: array[0..MAX_BONE_INFLUENCE - 1] of single;
-
-    class function Create: TVertexRec; static;
-    class function CreatePtr: PVertexRec; static;
-  end;
-
 procedure Test;
-type
-  T_int = array[0..MAX_BONE_INFLUENCE - 1] of integer;
-  P_int = ^T_int;
-
-  T_float = array[0..MAX_BONE_INFLUENCE - 1] of single;
-  P_float = ^T_float;
 var
-  i: integer;
-  j: PtrUInt;
-  tObj: TVertexObj;
-  tRec: TVertexRec;
-  Position: TVec3;
-  Normal: TVec3;
-  TexCoords: TVec2;
-  Tangent: TVec3;
-  Bitangent: TVec3;
-  BoneIDs: array[0..MAX_BONE_INFLUENCE - 1] of integer;
-  Weights: array[0..MAX_BONE_INFLUENCE - 1] of single;
-  p: Pointer;
-  p1, p2: Pointer;
-  pObj: PVertexObj;
-  pRec: PVertexRec;
+  a: array[0..3]
 begin
-  tObj := TVertexObj.Create;
-  pObj := TVertexObj.CreatePtr;
-  tRec := TVertexRec.Create;
-  pRec := TVertexRec.CreatePtr;
-
-  i := SizeOf(pObj^);
-  i := SizeOf(tObj);
-  i := SizeOf(pRec^);
-
-  p := pObj;
-  p1 := Pointer(0);
-  p2 := nil;
-
-  j := OffsetOf(pObj^, pObj^.Position);
-  Position := PVec3(p + j)^;
-  p1 := p1 + j;
-  p2 := Pointer(0) + j;
-
-  j := OffsetOf(pObj^, pObj^.BoneIDs);
-  BoneIDs := P_int(p + j)^;
-  p1 := p1 + j;
-  p2 := Pointer(0) + j;
-
-  j := OffsetOf(pObj^, pObj^.Weights);
-  Weights := P_float(p + j)^;
-  p1 := p1 + j;
-  p2 := Pointer(0) + j;
-
-  for j := 0 to 100 do
-  begin
-    Writeln(j);
-  end;
-
-  FreeObjAndNil(pObj);
-  FreeMemAndNil(pRec);
-
   Exit;
 end;
 
 procedure Run();
 begin
   Test;
-  //Main;
-end;
-
-{ TVertexRec }
-
-class function TVertexRec.Create: TVertexRec;
-begin
-  Result := Default(TVertexRec);
-
-  Result.Position := TVec3.Create(100, 100, 100);
-  Result.Normal := TVec3.Create(200, 200, 200);
-  Result.TexCoords := TVec2.Create(300, 300);
-  Result.Tangent := TVec3.Create(400, 400, 400);
-  Result.Bitangent := TVec3.Create(500, 500, 500);
-  Result.BoneIDs := [600, 600, 600, 600];
-  Result.Weights := [700, 700, 700, 700];
-end;
-
-class function TVertexRec.CreatePtr: PVertexRec;
-begin
-  New(Result);
-  Result^ := TVertexRec.Create;
-end;
-
-{ TVertexObj }
-
-constructor TVertexObj.Init;
-begin
-  inherited;
-
-  Self.Position := TVec3.Create(100, 100, 100);
-  Self.Normal := TVec3.Create(200, 200, 200);
-  Self.TexCoords := TVec2.Create(300, 300);
-  Self.Tangent := TVec3.Create(400, 400, 400);
-  Self.Bitangent := TVec3.Create(500, 500, 500);
-  Self.BoneIDs := [600, 600, 600, 600];
-  Self.Weights := [700, 700, 700, 700];
-end;
-
-class function TVertexObj.Create: TVertexObj;
-begin
-  Result.Init;
-end;
-
-class function TVertexObj.CreatePtr: PVertexObj;
-begin
-  new(Result, Init);
-end;
-
-destructor TVertexObj.Done;
-begin
-  inherited Done;
+  Main;
 end;
 
 end.
