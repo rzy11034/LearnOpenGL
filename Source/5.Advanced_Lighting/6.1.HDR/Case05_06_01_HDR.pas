a934acfb35e19910aaf73ceeef192048946ad4e1
BNC204-01 <rzy11034@163.com>
2024-09-13 19:43:37 +0800

    Signed-off-by: BNC204-01 <rzy11034@163.com>

--- Source/5.Advanced_Lighting/6.1.HDR/Case05_06_01_HDR.pas
+++ Source/5.Advanced_Lighting/6.1.HDR/Case05_06_01_HDR.pas
@@ -63,10 +63,11 @@ var
   hdrKeyPressed: Boolean = false;
   exposure: float = 1.0;
 
-  quadVAO: GLuint = 0;
-  quadVBO: GLuint = 0;
+  cubeVAO: Cardinal = 0;
+  cubeVBO: Cardinal = 0;
 
-  cubeVAO, cubeVBO: Cardinal;
+  quadVAO: Cardinal = 0;
+  quadVBO: Cardinal = 0;
 
 procedure Main;
 const
@@ -334,113 +335,108 @@ end;
 
 procedure RenderQuad;
 var
-  pos1, pos2, pos3, pos4, nm: TVec3;
-  tangent1, bitangent1, tangent2, bitangent2, edge1, edge2: TVec3;
-  uv1, uv2, uv3, uv4, deltaUV1, deltaUV2: TVec2;
-  f: float;
   quadVertices: TArr_GLfloat;
 begin
   if quadVAO = 0 then
   begin
-    // positions
-    pos1 := TGLM.Vec3(-1.0,  1.0, 0.0);
-    pos2 := TGLM.Vec3(-1.0, -1.0, 0.0);
-    pos3 := TGLM.Vec3( 1.0, -1.0, 0.0);
-    pos4 := TGLM.Vec3( 1.0,  1.0, 0.0);
-
-    // texture coordinates
-    uv1:= TGLM.Vec2(0.0, 1.0);
-    uv2:= TGLM.Vec2(0.0, 0.0);
-    uv3:= TGLM.Vec2(1.0, 0.0);
-    uv4:= TGLM.Vec2(1.0, 1.0);
-
-    // normal vector
-    nm := TGLM.Vec3(0.0, 0.0, 1.0);
-
-    // calculate tangent/bitangent vectors of both triangles
-    tangent1 := TGLM.Vec3(0);
-    bitangent1 := TGLM.Vec3(0);
-
-    tangent2 := TGLM.Vec3(0);
-    bitangent2:= TGLM.Vec3(0);
-
-    // triangle 1
-    // ----------
-    edge1 := pos2 - pos1;
-    edge2 := pos3 - pos1;
-    deltaUV1 := uv2 - uv1;
-    deltaUV2 := uv3 - uv1;
-
-    f := float(0);
-    f := 1.0 / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-    tangent1.x := f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-    tangent1.y := f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-    tangent1.z := f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-    tangent1 := TGLM.Normalize(tangent1);
-
-    bitangent1.x := f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-    bitangent1.y := f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-    bitangent1.z := f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-    bitangent1 := TGLM.Normalize(bitangent1);
-
-    // triangle 2
-    // ----------
-    edge1 := pos3 - pos1;
-    edge2 := pos4 - pos1;
-    deltaUV1 := uv3 - uv1;
-    deltaUV2 := uv4 - uv1;
-
-    f := 1.0 / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-
-    tangent2.x := f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
-    tangent2.y := f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
-    tangent2.z := f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
-    tangent2 := TGLM.Normalize(tangent2);
-
-    bitangent2.x := f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
-    bitangent2.y := f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
-    bitangent2.z := f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
-    bitangent2 := TGLM.Normalize(bitangent2);
-
     quadVertices := TArr_GLfloat([
-      // positions            // normal         // texcoords  // tangent                          // bitangent
-      pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-      pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
-      pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+      // positions        // texture Coords
+      -1.0,  1.0, 0.0,    0.0, 1.0,
+      -1.0, -1.0, 0.0,    0.0, 0.0,
+       1.0,  1.0, 0.0,    1.0, 1.0,
+       1.0, -1.0, 0.0,    1.0, 0.0]);
 
-      pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-      pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
-      pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
-    ]);
-
-    // configure plane VAO
+    // setup plane VAO
     glGenVertexArrays(1, @quadVAO);
     glGenBuffers(1, @quadVBO);
     glBindVertexArray(quadVAO);
     glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
     glBufferData(GL_ARRAY_BUFFER, quadVertices.MemSize, @quadVertices[0], GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * SIZE_OF_F, Pointer(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * SIZE_OF_F, Pointer(0));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * SIZE_OF_F, Pointer(3 * SIZE_OF_F));
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * SIZE_OF_F, Pointer(6 * SIZE_OF_F));
-    glEnableVertexAttribArray(3);
-    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * SIZE_OF_F, Pointer(8 * SIZE_OF_F));
-    glEnableVertexAttribArray(4);
-    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * SIZE_OF_F, Pointer(11 * SIZE_OF_F));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * SIZE_OF_F, Pointer(3 * SIZE_OF_F));
   end;
 
   glBindVertexArray(quadVAO);
-  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
   glBindVertexArray(0);
 end;
 
 procedure RenderCube;
+var
+  vertices: TArr_GLfloat;
 begin
-  cubeVAO := Cardinal(0);
-  cubeVBO := Cardinal(0);
+  if cubeVAO = 0 then
+  begin
+    vertices := TArr_GLfloat([
+      // back face
+      -1.0, -1.0, -1.0,  0.0,  0.0, -1.0, 0.0, 0.0, // bottom-left
+       1.0,  1.0, -1.0,  0.0,  0.0, -1.0, 1.0, 1.0, // top-right
+       1.0, -1.0, -1.0,  0.0,  0.0, -1.0, 1.0, 0.0, // bottom-right
+       1.0,  1.0, -1.0,  0.0,  0.0, -1.0, 1.0, 1.0, // top-right
+      -1.0, -1.0, -1.0,  0.0,  0.0, -1.0, 0.0, 0.0, // bottom-left
+      -1.0,  1.0, -1.0,  0.0,  0.0, -1.0, 0.0, 1.0, // top-left
+      // front face
+      -1.0, -1.0,  1.0,  0.0,  0.0,  1.0, 0.0, 0.0, // bottom-left
+       1.0, -1.0,  1.0,  0.0,  0.0,  1.0, 1.0, 0.0, // bottom-right
+       1.0,  1.0,  1.0,  0.0,  0.0,  1.0, 1.0, 1.0, // top-right
+       1.0,  1.0,  1.0,  0.0,  0.0,  1.0, 1.0, 1.0, // top-right
+      -1.0,  1.0,  1.0,  0.0,  0.0,  1.0, 0.0, 1.0, // top-left
+      -1.0, -1.0,  1.0,  0.0,  0.0,  1.0, 0.0, 0.0, // bottom-left
+      // left face
+      -1.0,  1.0,  1.0, -1.0,  0.0,  0.0, 1.0, 0.0, // top-right
+      -1.0,  1.0, -1.0, -1.0,  0.0,  0.0, 1.0, 1.0, // top-left
+      -1.0, -1.0, -1.0, -1.0,  0.0,  0.0, 0.0, 1.0, // bottom-left
+      -1.0, -1.0, -1.0, -1.0,  0.0,  0.0, 0.0, 1.0, // bottom-left
+      -1.0, -1.0,  1.0, -1.0,  0.0,  0.0, 0.0, 0.0, // bottom-right
+      -1.0,  1.0,  1.0, -1.0,  0.0,  0.0, 1.0, 0.0, // top-right
+      // right face
+       1.0,  1.0,  1.0,  1.0,  0.0,  0.0, 1.0, 0.0, // top-left
+       1.0, -1.0, -1.0,  1.0,  0.0,  0.0, 0.0, 1.0, // bottom-right
+       1.0,  1.0, -1.0,  1.0,  0.0,  0.0, 1.0, 1.0, // top-right
+       1.0, -1.0, -1.0,  1.0,  0.0,  0.0, 0.0, 1.0, // bottom-right
+       1.0,  1.0,  1.0,  1.0,  0.0,  0.0, 1.0, 0.0, // top-left
+       1.0, -1.0,  1.0,  1.0,  0.0,  0.0, 0.0, 0.0, // bottom-left
+      // bottom face
+      -1.0, -1.0, -1.0,  0.0, -1.0,  0.0, 0.0, 1.0, // top-right
+       1.0, -1.0, -1.0,  0.0, -1.0,  0.0, 1.0, 1.0, // top-left
+       1.0, -1.0,  1.0,  0.0, -1.0,  0.0, 1.0, 0.0, // bottom-left
+       1.0, -1.0,  1.0,  0.0, -1.0,  0.0, 1.0, 0.0, // bottom-left
+      -1.0, -1.0,  1.0,  0.0, -1.0,  0.0, 0.0, 0.0, // bottom-right
+      -1.0, -1.0, -1.0,  0.0, -1.0,  0.0, 0.0, 1.0, // top-right
+      // top face
+      -1.0,  1.0, -1.0,  0.0,  1.0,  0.0, 0.0, 1.0, // top-left
+       1.0,  1.0 , 1.0,  0.0,  1.0,  0.0, 1.0, 0.0, // bottom-right
+       1.0,  1.0, -1.0,  0.0,  1.0,  0.0, 1.0, 1.0, // top-right
+       1.0,  1.0,  1.0,  0.0,  1.0,  0.0, 1.0, 0.0, // bottom-right
+      -1.0,  1.0, -1.0,  0.0,  1.0,  0.0, 0.0, 1.0, // top-left
+      -1.0,  1.0,  1.0,  0.0,  1.0,  0.0, 0.0, 0.0  // bottom-left
+    ]);
+
+    glGenVertexArrays(1, @cubeVAO);
+    glGenBuffers(1, @cubeVBO);
+
+    // fill buffer
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.MemSize, vertices, GL_STATIC_DRAW);
+
+    // link vertex attributes
+    glBindVertexArray(cubeVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * SIZE_OF_F, Pointer(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * SIZE_OF_F, Pointer(3 * SIZE_OF_F));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * SIZE_OF_F, Pointer(6 * SIZE_OF_F));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+  end;
+
+  // render Cube
+  glBindVertexArray(cubeVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glBindVertexArray(0);
 end;
 
 procedure Framebuffer_size_callback(window: PGLFWwindow; witdth, Height: integer); cdecl;
